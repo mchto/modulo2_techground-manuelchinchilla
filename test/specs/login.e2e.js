a7073cfa3e1c74b6506b7ee885c8c4a9ad36f520
mchto <chinchi_007@hotmail.com>
2020-08-11 20:13:07 -0600

    creacion de timeouts, hooks, se crea las paginas waits y articleDetail

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,27 +1,33 @@
 const LoginPage = require('../pageobjects/login.page');
 const NavigationPage = require('../pageobjects/navigation.page');
 const DashboardPage = require('../pageobjects/dashboard.page');
+const waits = require('../helpers/waits');
+const articleDetailPage = require('../pageobjects/articleDetail.page');
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
-        NavigationPage.open();
+        browser.setTimeout({'pageLoad': 10000});
         NavigationPage.goToSignIn();
-        browser.pause(5000);
         LoginPage.login('manuelchinchilla@prueba.com','manuelChinchilla123');
+        browser.setTimeout({'implicit': 10000});
         expect(browser).toHaveUrl('https://demo.realworld.io/#/');
-        expect(DashboardPage.getNoArticlesLabel()).toBeDisplayed();
-        //expect(DashboardPage.getYourFeedTab()).toBeFocused();
         expect(DashboardPage.getYourFeedTab()).toHaveAttribute('class','nav-link active');
     });
 
     it('should open Global Feed tab', () => {
         DashboardPage.getGlobalFeedTab().click();
-        browser.pause(5000);
-        //console.log(DashboardPage.getArticlesList().lenght);
-        //expect(DashboardPage.getGlobalFeedTab()).toHaveAttribute('class','nav-link active');
+        browser.setTimeout({'implicit': 10000});
         expect(DashboardPage.getGlobalFeedTab()).toHaveClass('class',{ message: 'Not active tab!'});
-        expect(DashboardPage.getGlobalFeedTab()).not.toHaveProperty('height','24px');
-        //aqui valida que el tamaño de la lista sea igual a 10
-        expect(DashboardPage.getArticlesList()).toBeElementsArrayOfSize({ eq: 10});
+        expect(DashboardPage.getYourFeedTab()).toBeClickable();
     });
+
+    it('should open read more', () => {
+        DashboardPage.getReadMoreAnchor().click();
+    });
+
+    it('should add a descripcion', () => {
+        browser.setTimeout({'implicit': 10000});
+        articleDetailPage.getDescriptionTextBox().sendKeys('Hola');
+    });
+
 })
